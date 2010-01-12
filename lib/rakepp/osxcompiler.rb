ff26e9d4c430bd6d2f2323ddaf11cd49bd51c5de
gizmo <gizmo@b1134ee1-b69a-4768-92ac-d58ac0a408be>
2010-01-12 08:58:48 +0000

    name fuer shared libs
    git-svn-id: http://gizmomogwai.dyndns.org:8080/svn/ruby/trunk/RakePP@61 b1134ee1-b69a-4768-92ac-d58ac0a408be

--- lib/rakepp/osxcompiler.rb
+++ lib/rakepp/osxcompiler.rb
@@ -1,14 +1,19 @@
 class OsxCompiler < GccCompiler
+
   def initialize(defines, gui=true)
     super('osx', defines)
     @gui = gui
   end
+
   def startOfSharedLibCommand(libName)
-    return "g++ -dynamiclib #{ARCH}"
+    puts libName
+    return "g++ #{ARCH} -dynamiclib -install_name #{File.basename(libName)}"
   end
+
   def sharedExtension
     return 'so'
   end
+
   def addLib(task, lib)
     if (lib.instance_of?(Framework)) then
       return " -framework #{lib.name}"
@@ -22,4 +27,5 @@ class OsxCompiler < GccCompiler
       sh "/Developer/Tools/Rez -o #{artifact.outFile} appResources.r"
     end
   end
+
 end
--- lib/rakepp/sharedlib.rb
+++ lib/rakepp/sharedlib.rb
@@ -1,2 +1,9 @@
 class SharedLib < SourceLib
+  attr_reader :options
+
+  def initialize(compiler, base, name, sources, libs, includes, options={}, privateDefines=[])
+    super(compiler, base, name, sources, libs, include, privateDefines)
+    @options = options
+  end
+
 end
