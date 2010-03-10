43c3d879b172e492c5f8f6e51bacf8f11de72be7
gizmo <gizmo@b1134ee1-b69a-4768-92ac-d58ac0a408be>
2010-03-10 15:20:12 +0000

    testprogramme fuer crossbuilds
    git-svn-id: http://gizmomogwai.dyndns.org:8080/svn/ruby/trunk/RakePP@71 b1134ee1-b69a-4768-92ac-d58ac0a408be

---
+++ testrakepp/main1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <l1.h>
+int main(int argc, char** args) {
+  std::cout << "supi" << std::endl;
+  l1();
+  return 0;
+}
---
+++ testrakepp/main2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <l2.h>
+int main(int argc, char** args) {
+  std::cout << "supi" << std::endl;
+  l2();
+  return 0;
+}
