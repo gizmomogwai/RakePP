aab4e9a98a5fc68d95bf3489ee35f483415b9c91
gizmo <gizmo@b1134ee1-b69a-4768-92ac-d58ac0a408be>
2010-03-25 22:38:04 +0000

    fixed Rakefile.rb
    git-svn-id: http://gizmomogwai.dyndns.org:8080/svn/ruby/trunk/RakePP@72 b1134ee1-b69a-4768-92ac-d58ac0a408be

--- testrakepp/Rakefile.rb
+++ testrakepp/Rakefile.rb
@@ -11,7 +11,7 @@ base = 'libs/l2'
 sources = Files.relative(base, '*.cpp')
 l2 = SourceLib.new(compiler, base, 'l2', sources, [l1], ['.'])
 
-Exe.new(compiler, '.', 'test', Files.relative('./', '*.cpp'), [l2], ['.'])
+Exe.new(compiler, '.', 'test', Files.relative('./', 'main1.cpp'), [l2], ['.'])
 
 Cleaner.new
 
